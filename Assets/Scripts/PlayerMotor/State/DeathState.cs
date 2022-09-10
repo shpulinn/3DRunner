7303671fad3e7c93cd5d5487f8dc1832df656075
Nikita <shpulinn@mail.ru>
2022-09-10 10:33:02 +0200

    DeathState, RespawnState scripts created

---
+++ Assets/Scripts/PlayerMotor/State/DeathState.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class DeathState : BaseState
+{
+    [SerializeField] private Vector3 knockbackForce = new Vector3(0, 4, -3);
+
+    private Vector3 _currentKnockback;
+    
+    public override void Construct()
+    {
+        playerMotor.animator?.SetTrigger("Death");
+        _currentKnockback = knockbackForce;
+    }
+
+    public override Vector3 ProcessMotion()
+    {
+        //Vector3 move = knockbackForce;
+
+        _currentKnockback = new Vector3(0,
+            _currentKnockback.y - playerMotor.gravity * Time.deltaTime,
+            _currentKnockback.z += 2.0f * Time.deltaTime);
+
+        if (_currentKnockback.z > 0)
+        {
+            _currentKnockback.z = 0;
+            GameManager.Instance.ChangeState(GameManager.Instance.GetComponent<GameStateDeath>());
+        }
+
+        return _currentKnockback;
+    }
+}
---
+++ Assets/Scripts/PlayerMotor/State/RespawnState.cs
@@ -0,0 +1,56 @@
+using Inputs;
+using UnityEngine;
+
+public class RespawnState : BaseState
+{
+    [SerializeField] private float verticalDistance = 25.0f;
+    [SerializeField] private float immunityTime = 1.0f;
+
+    private float _startTime; 
+    
+    public override void Construct()
+    {
+        playerMotor.Controller.enabled = false;
+        playerMotor.transform.position = new Vector3(0, verticalDistance, playerMotor.transform.position.z);
+        playerMotor.Controller.enabled = true;
+
+        playerMotor.verticalVelocity = 0.0f;
+        playerMotor.ChangeLane(0);
+        playerMotor.animator?.SetTrigger("Respawn");
+
+        _startTime = Time.time;
+    }
+
+    public override Vector3 ProcessMotion()
+    {
+        // Apply gravity
+        playerMotor.ApplyGravity();
+
+        // Create return vector
+        Vector3 move = Vector3.zero;
+
+        move.x = playerMotor.SnapToLane();
+        move.y = playerMotor.verticalVelocity;
+        move.z = playerMotor.baseRunSpeed;
+
+        return move;
+    }
+
+    public override void Transition()
+    {
+        if (playerMotor.isGrounded && Time.time - _startTime > immunityTime)
+        {
+            playerMotor.ChangeState(GetComponent<RunningState>());
+        }
+        if (InputManager.Instance.SwipeLeft)
+        {
+            // Change lane to left (-1)
+            playerMotor.ChangeLane(-1);
+        }
+        if (InputManager.Instance.SwipeRight)
+        {
+            // Change lane to right (+1)
+            playerMotor.ChangeLane(1);
+        }
+    }
+}
